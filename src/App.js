6a2d2a2ff449dc3c2b2bcab41ba97f3a56947c4b
JessenFB <jessenfallikbemi_2327250065@mhs.mdp.ac.id>
2024-09-26 20:40:31 +0700

    p

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hello from './hello';
 import Product from './product';
 
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Hello nama= "Jessen Fallik Bemi" pesan="Selamat Datang Di Kelas IF31"/>
         <Hello/>
         <Product/>
+        <regi
       </header>
     </div>
   );
---
+++ src/registrasi.js
@@ -0,0 +1,25 @@
+import React from "react"; 
+ function Registrasi() { 
+    const [email, setEmail] = React.useState(""); 
+    const [nama, setNama]  = React.useState("");
+    const [hp, setHp] = React.useState(""); 
+        
+        const handleSubmit = (event) => {  
+            event.preventDefault(); 
+            // kirim ke server 
+            alert(`
+                nama : ${nama},
+                email : ${email},
+                nomor : ${hp}
+                `);
+                console.log(`
+                nama : ${nama},
+                email : ${email},
+                nomor : ${hp}
+                    `);
+                
+        }
+
+    };
+
+return Registrasi;
