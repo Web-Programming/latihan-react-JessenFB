fa931d790834da0a446758f4a1737a54e856bfd7
Erikaa311 <161033437+JessenFB@users.noreply.github.com>
2024-10-21 19:50:21 +0700

    p

--- latihan-axios-next/app/dashboard/page.js
+++ latihan-axios-next/app/dashboard/page.js
@@ -4,8 +4,8 @@ export default function Page() {
     return(
         <div>
             <h1>This is My Dashboard </h1>
-            <h2>Eddo Saputra</h2>
-            <h2>2327250007</h2>
+            <h2>Jessen Fallik Bemi</h2>
+            <h2>2327250065</h2>
             
         </div>
     );
